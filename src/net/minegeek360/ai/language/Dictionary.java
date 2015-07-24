77d2ffcc55e83420b1add11826f8c3823fe67f11
MightyElemental <minebox30@gmail.com>
2015-07-24 21:33:38 +0100

    Added javadoc to dictionary

--- src/net/minegeek360/ai/language/Dictionary.java
+++ src/net/minegeek360/ai/language/Dictionary.java
@@ -11,10 +11,13 @@ public class Dictionary {
 	public static final String[] presetQuestions = { "what", "where", "who", "when", "why", "how" };
 	public static final String[] presetYesNoQuestions = { "do", "can", "does", "is", "are" };
 	public static final String[] presetPropNouns = { "i", "london", "fairburn", "sherburn", "sydney", "washington", "amsterdam" };
-	public static final String[] presetNames = { "james", "noe", "evan",
-		"oliver", "reilly", "keean", "aaron", "john", "ahna", "ged", "joe", "fiona", "emma" };
-	
+	public static final String[] presetNames = { "james", "noe", "evan", "oliver", "reilly", "keean", "aaron", "john", "ahna", "ged", "joe",
+			"fiona", "emma" };
 
+	/** Adds a word to the word bank
+	 * 
+	 * @param word
+	 *            is the Word instance to be added to the word data bank */
 	public static void addWord(Word word) {
 		for (int i = 0; i < knownWords.length; i++) {
 			if (knownWords[i] != null) {
@@ -31,31 +34,26 @@ public class Dictionary {
 	public static Word[] getKnownWords() {
 		return knownWords;
 	}
-	
-	public static Word getWord(String string){
-		if (knownWordsKey.containsKey(string)) {
-			return knownWords[knownWordsKey.get(string)];
-		}
+
+	public static Word getWord(String string) {
+		if (knownWordsKey.containsKey(string)) { return knownWords[knownWordsKey.get(string)]; }
 		return null;
 	}
 
 	public static Map<String, String> getWordTags(String string) {
-		if (knownWordsKey.containsKey(string)) {
-			return knownWords[knownWordsKey.get(string)].getTags();
-		}
+		if (knownWordsKey.containsKey(string)) { return knownWords[knownWordsKey.get(string)].getTags(); }
 		return null;
 	}
 
 	public static String[] getWordTagsArray(String string) {
 		return (String[]) getWordTags(string).entrySet().toArray();
 	}
-	
-	/**Returns all the words with the given tag*/
+
+	/** Returns all the words with the given tag */
 	public static ArrayList<String> getAllWordsWithTag(String tag) {
 		ArrayList<String> temp = new ArrayList<String>();
 		for (int i = 0; i < knownWords.length; i++) {
-			if ((knownWords[i] != null)
-					&& (knownWords[i].getTags().containsKey(tag))) {
+			if ((knownWords[i] != null) && (knownWords[i].getTags().containsKey(tag))) {
 				temp.add(knownWords[i].toString());
 			}
 		}
